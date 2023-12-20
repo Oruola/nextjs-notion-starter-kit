20a139f7af3bd4b66d6e0429b4d68894a5ee08ed
阿海 <xu15222379857@gmail.com>
2023-12-20 16:50:05 +0800

    Limm pages

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'ad64d45f39f54d449d9c2e08fc918755',
+  rootNotionPageId: '8f88523c4dda4baeb28f60d296fa911d',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: '9d31172a-4df0-404a-98cc-0775c52bad0d',
+  rootNotionSpaceId: '',
 
   // basic site info (required)
-  name: '阿呆 Next.js Notion Starter Kit',
+  name: '李每每 Next.js Notion Starter Kit',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: '阿呆',
+  author: '李每每',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: '阿呆',
-  github: 'Oruola',
-  linkedin: 'Oruola',
+  twitter: '#',
+  github: '#',
+  linkedin: '#',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
